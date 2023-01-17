6799d9b68b4a075eb8212275524e68535decdce7
James Wasmer <jameswasmer@gmail.com>
2023-01-17 12:23:59 -0500

    Repair broken Orders syntax

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Orders.css';
 
-export default function Orders({ orders }) {
-  const orderEls = orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  })
+export default function Orders({ orders }) { 
+
+  const orderMaker = (orders) => {
+    if (orders) {
+      return  orders.map(order => {
+        return (
+          <div className="order">
+            <h3>{order.name}</h3>
+            <ul className="ingredient-list">
+              {order.ingredients.map(ingredient => {
+                return <li>{ingredient}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })
+    }
+  }
+
+  
+
   return (
     <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
+      { orderMaker() ? orderMaker() : <p>No orders yet!</p> }
     </section>
   )
 }
