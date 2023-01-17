35541878f85646f741c79ed76aacd31962440e3f
James Wasmer <jameswasmer@gmail.com>
2023-01-17 12:24:45 -0500

    Build POST request for OrderForm

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,20 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrders = (data) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
